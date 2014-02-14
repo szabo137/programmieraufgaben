1c3be6b8c18bf94b76969626b0a45eeb204347b7
szabo137 <xkj@gmx.de>
2014-02-14 09:08:20 +0100

    add beginning of a programm about removing points out of data names.

---
+++ pkteausdateinamen/punkteausdateinamen.py
@@ -0,0 +1,9 @@
+"""
+Es werden Dateinamen aus dem Ordner ausgelesen und aus allen werden die Punkte bis 
+auf den Punkt vor der Dateiendung entfernt.
+
+http://www.programmieraufgaben.ch/aufgabe/punkte-aus-dateinamen-entfernen/cxrshndx
+
+"""
+import os
+
